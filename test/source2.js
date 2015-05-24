6dd56416e221a139a9518318af97885d5019dd86
hushicai <bluthcy@gmail.com>
2015-05-24 21:43:20 +0800

    update

---
+++ test/source2.js
@@ -0,0 +1,14 @@
+/**
+ * @file sourc2
+ * @author hushicai(bluthcy@gmail.com)
+ */
+
+var test = {
+    x: function () {
+        return 1;
+    },
+    y: function () {
+        var a = 1;
+        return a;
+    }
+};
